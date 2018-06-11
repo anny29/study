d9f047f9efe32239c3bcf7c662c31b6f5a65601d
anny29 <lk753951@hotmail.com>
2018-06-12 07:14:27 +0800

    列表练习

--- python/programs/chapter3_list/list_practice.py
+++ python/programs/chapter3_list/list_practice.py
@@ -3,4 +3,20 @@ names = ["Hui", "Lei", "Min", "Yi"];
 for i in range(4):
     print("Hi " + names[i])
 
+#3-4 
+print("\n3-4")
+guests = []
+guests.append('Father')
+guests.append('Mother')
+guests.append('Wife')
+guests.append('cusion')
+for i in range(len(guests)):
+	print('I love you, ' + guests[i])
+
+print('\n3-5')
+print(guests[-1] + " is not here")
+guests[-1] = "child"
+for i in range(len(guests)):
+	print('I love you, ' + guests[i])
+
 
