f32d44d49052815faf953ed5a03842dd34ae22fc
anny29 <lk753951@hotmail.com>
2018-06-12 22:10:06 +0800

    列表增删改练习

--- python/programs/chapter3_list/list_practice.py
+++ python/programs/chapter3_list/list_practice.py
@@ -13,10 +13,38 @@ guests.append('cusion')
 for i in range(len(guests)):
 	print('I love you, ' + guests[i])
 
+#3-5
 print('\n3-5')
 print(guests[-1] + " is not here")
 guests[-1] = "child"
 for i in range(len(guests)):
 	print('I love you, ' + guests[i])
+
+#3-6 新增 修改
+print("\n3-6")
+print("more person")
+guests.insert(0, "Ma Ma")
+guests.insert(2, "Ba Ba")
+guests.append("Ye Ye")
+for i in range(len(guests)):
+	print("Be Happy " + guests[i])
+
+#3-7 删除
+print("\n3-7")
+print("only two left")
+for i in range(0, len(guests) - 2):
+	person = guests.pop()
+	print("Sorry " + person)
+
+for i in range(len(guests)):
+	print("You are here: " + guests[i])
+
+print("delete left")
+print(guests)
+
+for i in range(len(guests)):
+	del guests[0]
+
+print(guests)
 
 
